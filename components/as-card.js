44544326dfcdae2d6fedb1bb5f75171e156542fc
arin-s <sarkisianarin@gmail.com>
2025-01-10 16:00:13 +1100

    Refactored both elements to work using web components and events.

--- components/as-card.js
+++ components/as-card.js
@@ -1,15 +1,48 @@
-"use strict"
-
+"use strict";
 class Card extends HTMLElement {
+  shadowRoot = "sd";
+  top;
+  left;
+  rotate;
+  state;
 
   constructor() {
     super();
-
+    this.shadowRoot = this.attachShadow({ mode: "open" });
+    let template = document.createElement("template");
+    template.innerHTML = `
+      <link rel="stylesheet" href="components/as-card.css">
+      <div class="card-focus-field">
+        <div class="rotate-x">
+          <div class="rotate-y card"></div>
+        </div>
+      </div>
+    `;
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.addEventListener("get-width", () => {
+      let width = this.shadowRoot.querySelector(".card").clientWidth;
+      this.dispatchEvent(
+        new CustomEvent("send-width", {
+          composed: true, bubbles: true,
+          detail: { width: width },
+        })
+      );
+    });
+    this.addEventListener("set-pos", (event) => {
+      this.top = event.detail.top;
+      this.left = event.detail.left;
+      this.rotate = event.detail.rotate;
+      this.tryUpdatePos();
+    });
   }
+  tryUpdatePos() {
+      //Add state handling code
+      if(this.state === "onPath") {
 
-  //Runs every time the element is added to the document
-  connectedCallback() {
+      }
+      if(this.state === "onPath") {
 
+      }
   }
 }
 
--- components/as-deck.js
+++ components/as-deck.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 class Deck extends HTMLElement {
   PATH_HEIGHT = 250;
@@ -8,98 +8,114 @@ class Deck extends HTMLElement {
   svg;
   path;
   cards = [];
+  shadowRoot;
+  slot;
 
   constructor() {
     super();
-
+    this.shadowRoot = this.attachShadow({ mode: "open" });
+    let template = document.createElement("template");
+    template.innerHTML = `
+      <svg>
+        <path d="" stroke="black" fill="transparent" />
+      </svg>
+      <slot></slot>
+    `;
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.slot = this.shadowRoot.lastElementChild;
+    this.slot.addEventListener("slotchange", (e) => {
+      //this.updatePath();
+    });
+    document.addEventListener("readystatechange", this.onLoad.bind(this));
   }
 
   //Runs every time the element is added to the document
   connectedCallback() {
-    this.cards = [...this.children];
-    this.insertAdjacentHTML("afterbegin",
-      `<svg id="svg">
-        <path id="path" d="" stroke="black" fill="transparent"/>
-      </svg>`);
-    this.svg = this.firstElementChild;
+    this.svg = this.shadowRoot.firstElementChild;
     this.path = this.svg.firstElementChild;
-    this.fixPath();
-    window.addEventListener("resize", this.fixPath);
+    window.addEventListener("resize", this.updatePath.bind(this));
   }
 
-  //Recreates and then sets the SVG path
-  fixPath() {
+  //Event listener that fires when finished loading
+  async onLoad() {
+    //Only fire when styles are loaded
+    if (document.readyState !== "complete") return;
     //Get child cards
-    this.cards = [...this.children].filter((elem) => elem.tagName === "AS-CARD");
-    //If there are no cards, return
-    if(this.cards.length === 0) {
+    this.cards = this.slot
+      .assignedElements()
+      .filter((elem) => elem.tagName === "AS-CARD");
+    //If there are no cards, warn
+    if (this.cards.length === 0) {
       console.warn("No cards detected within <as-deck>, intended?");
       return;
     }
-    let cardWidth = this.cards[0].offsetWidth;
-    let startX = this.svg.clientWidth / 2 - cardWidth * this.cards.length / 2;
-    let endX = this.svg.clientWidth / 2 + cardWidth * this.cards.length / 2;
-    this.path.setAttribute("d", `M ${startX} ${this.svg.clientHeight + this.HEIGHT_OFFSET} C ` +
-      `${startX - cardWidth * this.BENDYNESS} ${this.svg.clientHeight - this.PATH_HEIGHT + this.HEIGHT_OFFSET}, ` +
-      `${endX + cardWidth * this.BENDYNESS} ${this.svg.clientHeight - this.PATH_HEIGHT + this.HEIGHT_OFFSET}, ` + 
-      `${endX} ${this.svg.clientHeight + this.HEIGHT_OFFSET}`);
-    //fixAllPos();
+    //Request card width
+    let cardWidth = null;
+    await new Promise((resolve, reject) => {
+      this.addEventListener("send-width", (event) => {
+        cardWidth = event.detail.width;
+        event.stopPropagation();
+        resolve();
+      });
+
+      this.cards[0].dispatchEvent(new Event("get-width", { composed: true }));
+      setTimeout(() => reject("Child card failed to answer"), 3000);
+    });
+    if (cardWidth === null) return;
+    this.updatePath();
   }
 
-  //need custom events for requesting card coords
-
-  fixAllPos() {
-    for (let card of cards)
-      fixPos(card);
+  //Recreates and then sets the SVG path
+  updatePath() {
+    console.log("sssssssssssddd");
+    let card = this.slot
+      .assignedElements()[0]
+      .shadowRoot.querySelector(".card");
+    let cardWidth = card.offsetWidth;
+    let startX = this.svg.clientWidth / 2 - (cardWidth * this.cards.length) / 2;
+    let endX = this.svg.clientWidth / 2 + (cardWidth * this.cards.length) / 2;
+    this.path.setAttribute(
+      "d",
+      `M ${startX} ${this.svg.clientHeight + this.HEIGHT_OFFSET} C ` +
+        `${startX - cardWidth * this.BENDYNESS} ${
+          this.svg.clientHeight - this.PATH_HEIGHT + this.HEIGHT_OFFSET
+        }, ` +
+        `${endX + cardWidth * this.BENDYNESS} ${
+          this.svg.clientHeight - this.PATH_HEIGHT + this.HEIGHT_OFFSET
+        }, ` +
+        `${endX} ${this.svg.clientHeight + this.HEIGHT_OFFSET}`
+    );
+    for (let card of this.cards) this.updateCard(card);
   }
 
   SPACING_MULT = 1.3;
-  fixPos(card) {
-    let arcLength = card.offsetWidth * SPACING_MULT;
-    let cardIndex = Object.keys(cards).find(key => cards[key] === card);
-    let startLength = path.getTotalLength() / 2 - arcLength * (cards.length - 1) / 2;
+  updateCard(card) {
+    let top, left, rotate;
+    let arcLength = card.offsetWidth * this.SPACING_MULT;
+    let cardIndex = Object.keys(this.cards).find(
+      (key) => this.cards[key] === card
+    );
+    let startLength =
+      this.path.getTotalLength() / 2 -
+      (arcLength * (this.cards.length - 1)) / 2;
     let targetLength = startLength + arcLength * cardIndex;
-    let targetPoint = path.getPointAtLength(targetLength);
+    let targetPoint = this.path.getPointAtLength(targetLength);
     let deckRect = this.svg.getBoundingClientRect();
-    card.style.top = `${deckRect.top + targetPoint.y - card.offsetHeight / 2}px`;
-    card.style.left = `${deckRect.left + targetPoint.x - card.offsetWidth / 2}px`;
+    top = `${
+      deckRect.top + targetPoint.y - card.offsetHeight / 2
+    }px`;
+    left = `${
+      deckRect.left + targetPoint.x - card.offsetWidth / 2
+    }px`;
     //translation code
-    let p1 = path.getPointAtLength(targetLength - card.offsetWidth / 2);
-    let p2 = path.getPointAtLength(targetLength + card.offsetWidth / 2);
+    let p1 = this.path.getPointAtLength(targetLength - card.offsetWidth / 2);
+    let p2 = this.path.getPointAtLength(targetLength + card.offsetWidth / 2);
     let radians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
-    card.style.rotate = `${radians * (180 / Math.PI)}deg`;
+    rotate = `${radians * (180 / Math.PI)}deg`;
+    this.cards[0].dispatchEvent(
+      new CustomEvent("set-pos", { detail: {left: left, top: top, rotate: rotate}, composed: true })
+    );
   }
 }
 
 customElements.define("as-deck", Deck);
-
-
-/*
-//Declarations
-let this.svg = document.getElementById("this.svg");
-let path = document.getElementById("path");
-
-
-//Cards
-function cardManagerFactory() {
-  let cards = [];
-
-
-  function addCard(title, img, link) {
-    let card = document.createElement("div");
-    document.getElementsByTagName("body")[0].appendChild(card);
-    card.classList.add("card");
-    card.innerText = title;
-    cards.push(card);
-    fixPath();
-  }
-
-
-
-
-
-
-
-  return { addCard, fixPath, cards };
-}
-*/
